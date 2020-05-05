968bc9ac6f97f85dd48e41386e4b30e76c10b054
AndreFukushigue <andre.fukushigue@gmail.com>
2020-05-05 20:55:52 -0300

    Mensagens extras

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Teste");
-
+		System.out.println("AAAAAAAAAAA");
+		System.out.println("Have you ever wondered what it's like, to be a wanderer in the 4th dimension?");
 	}
-
 }
